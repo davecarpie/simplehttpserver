9ad13b01a4354bc37bc211a1f4f5d3bf3f3211f6
Dave Carpenter <dave.s.carpenter@gmail.com>
2015-05-12 12:31:34 +1200

    hide hidden files that start with a period

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var fs = require('fs')
 var http = require('http')
 
+var serverDirectory = process.argv[2]
+
 var server = http.createServer(function(req, res) {
-  fs.readdir(".", function(err, files) {
+  fs.readdir(serverDirectory, function(err, files) {
     if (err) {
       return res.end("Error loading files")
     }
 
     files.forEach(function(fileName) {
-      res.write(fileName + "\n");
+      if (fileName.charAt(0) !== ".")
+        res.write(fileName + "\n");
     })
 
     return res.end()
