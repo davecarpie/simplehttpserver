bf53b45613386e7a2bf11c949f036d3213ca5c93
Dave Carpenter <dave.s.carpenter@gmail.com>
2015-05-12 12:59:25 +1200

    server now renders html with links for each file

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 var fs = require('fs')
 var http = require('http')
+var url = require('url')
 
 var serverDirectory = process.argv[2]
 
 var server = http.createServer(function(req, res) {
+  var reqUrl = url.parse(req.url)
+  console.log(reqUrl)
+
   fs.readdir(serverDirectory, function(err, files) {
     if (err) {
       return res.end("Error loading files")
     }
 
+    res.write("<h1>Contents of " + serverDirectory + "</h1><hr /><ul>")
+
     files.forEach(function(fileName) {
-      if (fileName.charAt(0) !== ".")
-        res.write(fileName + "\n");
+      if (fileName.charAt(0) !== ".") {
+        filePath = reqUrl.path + fileName
+        res.write("<li><a href=\"" + filePath + "\">" + fileName + "</a></li>");
+      }
     })
 
-    return res.end()
+    return res.end("</ul><hr />")
 
   })
 });
