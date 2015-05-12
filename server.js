98d8b881534182d41a94cbd6f0b6aebf32a08e79
Dave Carpenter <dave.s.carpenter@gmail.com>
2015-05-13 10:08:02 +1200

    now serves files correctly but has issue with files with spaces in their name

--- server.js
+++ server.js
@@ -1,42 +1,54 @@
 var fs = require('fs')
 var http = require('http')
 var url = require('url')
+var isFile = require('./isFile')
 
 var baseDirectory = process.argv[2]
 
 var server = http.createServer(function(req, res) {
   var urlObj = url.parse(req.url)
-  console.log(urlObj)
-  var dirPath = baseDirectory + urlObj.path
+  var currPath = baseDirectory + urlObj.path
 
-  fs.readdir(dirPath, function(err, fileNames) {
-    if (err) {
+  if (urlObj.path === "/favicon.ico") {
+    return
+  };
+
+  isFile(currPath, function(err, pathIsFile) {
+    if (err)
       return res.end("Error loading files")
-    }
 
-    res.write("<h1>Contents of " + dirPath + "</h1><hr /><ul>")
-
-    var folders = []
-    var files = []
-
-    fileNames.forEach(function(fileName) {
-      if (fileName.charAt(0) !== ".") {
-        var fileStat = fs.statSync(dirPath+fileName)
-        if (fileStat.isDirectory()) {
-          folders.push(fileName + "/")
-        } else {
-          files.push(fileName)
+    if (pathIsFile) {
+      fs.createReadStream(currPath).pipe(res)
+    } else {
+      fs.readdir(currPath, function(err, fileNames) {
+        if (err) {
+          return res.end("Error loading files")
         }
-      }
-    })
 
-    folders.concat(files).forEach(function(fileName) {
-      filePath =  urlObj.path + fileName
-      res.write("<li><a href=\"" + filePath + "\">" + fileName + "</a></li>");
-    })
+        res.write("<h1>Contents of " + currPath + "</h1><hr /><ul>")
 
-    return res.end("</ul><hr />")
+        var folders = []
+        var files = []
 
+        fileNames.forEach(function(fileName) {
+          if (fileName.charAt(0) !== ".") {
+            var fileStat = fs.statSync(currPath+fileName)
+            if (fileStat.isDirectory()) {
+              folders.push(fileName + "/")
+            } else {
+              files.push(fileName)
+            }
+          }
+        })
+
+        folders.concat(files).forEach(function(fileName) {
+          filePath =  urlObj.path + fileName
+          res.write("<li><a href=\"" + filePath + "\">" + fileName + "</a></li>");
+        })
+
+        return res.end("</ul><hr />")    
+      })
+    }
   })
 });
 
